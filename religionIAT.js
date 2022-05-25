98dd690578e523f6769c7b485dd7b22280f9a4da
Nandant11 <30779435+Nandant11@users.noreply.github.com>
2022-05-25 03:20:30 -0400

    Update religionIAT.js

--- religionIAT.js
+++ religionIAT.js
@@ -45,51 +45,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
             ],
             //Stimulus css (style)
             stimulusCss : {color:'#31940F','font-size':'2.3em'}
-        },
-        attribute1 : {
-            name : 'Bad',
-            title : {
-                media : {word : 'Bad'},
-                css : {color:'#0000FF','font-size':'1.8em'},
-                height : 4 //Used to position the "Or" in the combined block.
-            },
-            stimulusMedia : [ //Stimuli content as PIP's media objects
-                {word: global.negWords[0]},
-                {word: global.negWords[1]},
-                {word: global.negWords[2]},
-                {word: global.negWords[3]},
-                {word: global.negWords[4]},
-                {word: global.negWords[5]},
-                {word: global.negWords[6]},
-                {word: global.negWords[7]}
-
-            ],
-            //Stimulus css
-            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
-        },
-        attribute2 : {
-            name : 'Good',
-            title : {
-                media : {word : 'Good'},
-                css : {color:'#0000FF','font-size':'1.8em'},
-                height : 4 //Used to position the "Or" in the combined block.
-            },
-            stimulusMedia : [ //Stimuli content as PIP's media objects
-                {word: global.posWords[0]},
-                {word: global.posWords[1]},
-                {word: global.posWords[2]},
-                {word: global.posWords[3]},
-                {word: global.posWords[4]},
-                {word: global.posWords[5]},
-                {word: global.posWords[6]},
-                {word: global.posWords[7]}
-            ],
-            //Stimulus css
-            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
-        },
-
-        isTouch : global.$isTouch
-    });
+       
 
 });
 
