7c3206640609b2c3213e5d2589cc40d19ab23931
Nandant11 <30779435+Nandant11@users.noreply.github.com>
2022-05-26 05:36:47 -0400

    Add files via upload

--- religionIAT.js
+++ religionIAT.js
@@ -1,114 +1,96 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
-    var API = new APIConstructor();
-var posWords = API.shuffle([
-            'Love', 'Cheer', 'Friend', 'Pleasure', 
-            'Adore', 'Cheerful', 'Friendship', 'Joyful', 
-            'Smiling','Cherish', 'Excellent', 'Glad', 
-            'Joyous', 'Spectacular', 'Appealing', 'Delight', 
-            'Excitement', 'Laughing', 'Attractive','Delightful', 
-            'Fabulous', 'Glorious', 'Pleasing', 'Beautiful', 
-            'Fantastic', 'Happy', 'Lovely', 'Terrific', 
-            'Celebrate', 'Enjoy', 'Magnificent', 'Triumph']);
-  var negWords = API.shuffle([
-            'Abuse', 'Grief', 'Poison', 'Sadness', 
-            'Pain', 'Despise', 'Failure', 'Nasty', 
-            'Angry', 'Detest', 'Horrible', 'Negative', 
-            'Ugly', 'Dirty', 'Gross', 'Evil', 
-            'Rotten','Annoy', 'Disaster', 'Horrific',  
-            'Scorn', 'Awful', 'Disgust', 'Hate', 
-            'Humiliate', 'Selfish', 'Tragic', 'Bothersome', 
-            'Hatred', 'Hurtful', 'Sickening', 'Yucky']);
-	return iatExtension({
+define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat7.js'], function(APIConstructor, iatExtension){
+    let API = new APIConstructor();
+    let global = API.getGlobal();
 
-		category1 : {
-			name : 'Arab Muslims', //Will appear in the data.
-			title : {
-				media : {word : 'Arab Muslims'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
-				height : 4 //Used to position the "Or" in the combined block.
-			}, 
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		        {word: 'Hakim'},
-                {word: 'Sharif'},
-                {word: 'Yousef'},
-				        {word: 'Wahib'},
-            	  {word: 'Akbar'},
-            	  {word: 'Muhsin'},
-            	  {word: 'Salim'},
+    return iatExtension({
+        category1 : {
+            name : 'Muslim', //Will appear in the data.
+            title : {
+                media : {word : 'Muslim'}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {word: 'Khan'},
+                {word: 'Hussain'},
+                {word: 'Ali'},
+                {word: 'Ahmad'},
+                {word: 'Allah'},
+                {word: 'Asif'},
+                {word: 'Qadri'},
                 {word: 'Karim'},
                 {word: 'Habib'},
-				        {word: 'Ashraf'}  
-			], 
-			//Stimulus css (style)
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},	
-		category2 :	{
-			name : 'Other People', //Will appear in the data.
-			title : {
-				media : {word : 'Other People'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
-				height : 4 //Used to position the "Or" in the combined block.
-			}, 
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		        {word: 'Ernesto'},
-                {word: 'Matthais'},
-                {word: 'Maarten'},
-				        {word: 'Philippe'},
-            	  {word: 'Guillame'},
-            	  {word: 'Benoit'},
-            	  {word: 'Takuya'},
-                {word: 'Kazuki'},
-                {word: 'Chaiyo'},
-				        {word: 'Marcelo'}
-			], 
-			//Stimulus css
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},
-		attribute1 :
-		{
-			name : 'Bad',
-			title : {
-				media : {word : 'Bad'},
-				css : {color:'#0000FF','font-size':'1.8em'},
-				height : 4 //Used to position the "Or" in the combined block.
-			},
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-				{word: negWords[0]},
-				{word: negWords[1]},
-				{word: negWords[2]},
-				{word: negWords[3]},
-				{word: negWords[4]},
-				{word: negWords[5]},
-				{word: negWords[6]},
-				{word: negWords[7]}
-			],
-			//Stimulus css
-			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
-		},
-		attribute2 :
-		{
-			name : 'Good',
-			title : {
-				media : {word : 'Good'},
-				css : {color:'#0000FF','font-size':'1.8em'},
-				height : 4 //Used to position the "Or" in the combined block.
-			},
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-				{word: posWords[0]},
-				{word: posWords[1]},
-				{word: posWords[2]},
-				{word: posWords[3]},
-				{word: posWords[4]},
-				{word: posWords[5]},
-				{word: posWords[6]},
-				{word: posWords[7]}
-			],
-			//Stimulus css
-			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
-		},
+                {word: 'Ashraf'}  
+            ],
+            //Stimulus css (style)
+            stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },    
+        category2 :    {
+            name : 'Hindu', //Will appear in the data.
+            title : {
+                media : {word : 'Hindu'}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {word: 'Patel'},
+                {word: 'Desai'},
+                {word: 'Bhavsar'},
+                {word: 'Thakor'},
+                {word: 'Joshi'},
+                {word: 'Mehta'},
+                {word: 'Prasad'},
+                {word: 'Rathod'},
+                {word: 'Parekh'},
+                {word: 'Acharya'}
+            ],
+            //Stimulus css (style)
+            stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },
+        attribute1 : {
+            name : 'Bad',
+            title : {
+                media : {word : 'Bad'},
+                css : {color:'#0000FF','font-size':'1.8em'},
+                height : 4 //Used to position the "Or" in the combined block.
+            },
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {word: global.negWords[0]},
+                {word: global.negWords[1]},
+                {word: global.negWords[2]},
+                {word: global.negWords[3]},
+                {word: global.negWords[4]},
+                {word: global.negWords[5]},
+                {word: global.negWords[6]},
+                {word: global.negWords[7]}
 
-		base_url : {//Where are your images at?
-			image : 'https://baranan.github.io/minno-tasks/images/'
-		} 
-	});
+            ],
+            //Stimulus css
+            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+        },
+        attribute2 : {
+            name : 'Good',
+            title : {
+                media : {word : 'Good'},
+                css : {color:'#0000FF','font-size':'1.8em'},
+                height : 4 //Used to position the "Or" in the combined block.
+            },
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {word: global.posWords[0]},
+                {word: global.posWords[1]},
+                {word: global.posWords[2]},
+                {word: global.posWords[3]},
+                {word: global.posWords[4]},
+                {word: global.posWords[5]},
+                {word: global.posWords[6]},
+                {word: global.posWords[7]}
+            ],
+            //Stimulus css
+            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+        },
+
+        isTouch : global.$isTouch
+    });
+
 });
+
