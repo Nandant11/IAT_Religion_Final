907ec27dd7f176383b12f19cd67db170d28e869f
Nandant11 <30779435+Nandant11@users.noreply.github.com>
2022-07-10 17:09:27 +0530

    Update IAT_Tablet.js

--- IAT_Tablet.js
+++ IAT_Tablet.js
@@ -19,9 +19,9 @@ var posWords = API.shuffle([
 	return iatExtension({
 
 		category1 : {
-			name : 'Muslims', //Will appear in the data.
+			name : 'Muslim people', //Will appear in the data.
 			title : {
-				media : {word : 'Muslims'}, //Name of the category presented in the task.
+				media : {word : 'Muslim people'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
@@ -37,9 +37,9 @@ var posWords = API.shuffle([
 			stimulusCss : {color:'#31940F','font-size':'2.3em'}
 		},	
 		category2 :	{
-			name : 'Hindus', //Will appear in the data.
+			name : 'Hindu people', //Will appear in the data.
 			title : {
-				media : {word : 'Hindus'}, //Name of the category presented in the task.
+				media : {word : 'Hindu people'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
